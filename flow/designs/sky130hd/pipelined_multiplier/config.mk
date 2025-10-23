5a6cf19e2fe96f70f0e7af3668c74408c9be981d
Blueonics <lannan.jiang@gmail.com>
2025-10-23 20:58:36 +0000

    set up design config files

---
+++ flow/designs/sky130hd/pipelined_multiplier/config.mk
@@ -0,0 +1,32 @@
+export DESIGN_NICKNAME = pipelined_multiplier
+export DESIGN_NAME = pipeline
+export PLATFORM    = sky130hd
+
+export VERILOG_FILES = \
+    $(DESIGN_HOME)/src/$(DESIGN_NICKNAME)/src/half_adder.v \
+    $(DESIGN_HOME)/src/$(DESIGN_NICKNAME)/src/rca_1b.v \
+    $(DESIGN_HOME)/src/$(DESIGN_NICKNAME)/src/rca_4b.v \
+    $(DESIGN_HOME)/src/$(DESIGN_NICKNAME)/src/rca_4b1.v \
+    $(DESIGN_HOME)/src/$(DESIGN_NICKNAME)/src/rca8b.v \
+    $(DESIGN_HOME)/src/$(DESIGN_NICKNAME)/src/rca16b.v \
+    $(DESIGN_HOME)/src/$(DESIGN_NICKNAME)/src/rca32b.v \
+    $(DESIGN_HOME)/src/$(DESIGN_NICKNAME)/src/rca64.v \
+    $(DESIGN_HOME)/src/$(DESIGN_NICKNAME)/src/vdcmul_2b.v \
+    $(DESIGN_HOME)/src/$(DESIGN_NICKNAME)/src/vdcmul_4b.v \
+    $(DESIGN_HOME)/src/$(DESIGN_NICKNAME)/src/vdcmul_8b.v \
+    $(DESIGN_HOME)/src/$(DESIGN_NICKNAME)/src/vdcmul_16b.v \
+    $(DESIGN_HOME)/src/$(DESIGN_NICKNAME)/src/vdcmul_32b.v \
+    $(DESIGN_HOME)/src/$(DESIGN_NICKNAME)/src/vdcmul_64b.v \
+    $(DESIGN_HOME)/src/$(DESIGN_NICKNAME)/pipeline.v
+
+export SDC_FILE      = $(DESIGN_HOME)/$(PLATFORM)/$(DESIGN_NICKNAME)/constraint.sdc
+
+
+export CORE_UTILIZATION = 45
+export PLACE_DENSITY_LB_ADDON = 0.2
+export TNS_END_PERCENT = 100
+
+export FASTROUTE_TCL = $(DESIGN_HOME)/$(PLATFORM)/$(DESIGN_NICKNAME)/fastroute.tcl
+
+export REMOVE_ABC_BUFFERS = 1
+export HDL_FRONTEND = slang
---
+++ flow/designs/sky130hd/sobel_filter/config.mk
@@ -0,0 +1,20 @@
+export DESIGN_NICKNAME = sobel_filter
+export DESIGN_NAME = sobel
+export PLATFORM    = sky130hd
+
+export VERILOG_FILES = \
+	$(DESIGN_HOME)/src/$(DESIGN_NICKNAME)/sliding_window.sv \
+	$(DESIGN_HOME)/src/$(DESIGN_NICKNAME)/sobel_filter.sv
+     
+
+export SDC_FILE      = $(DESIGN_HOME)/$(PLATFORM)/$(DESIGN_NICKNAME)/constraint.sdc
+
+
+export CORE_UTILIZATION = 45
+export PLACE_DENSITY_LB_ADDON = 0.2
+export TNS_END_PERCENT = 100
+
+export FASTROUTE_TCL = $(DESIGN_HOME)/$(PLATFORM)/$(DESIGN_NICKNAME)/fastroute.tcl
+
+export REMOVE_ABC_BUFFERS = 1
+export HDL_FRONTEND = slang
