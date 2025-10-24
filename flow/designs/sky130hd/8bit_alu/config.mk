d74d4c18ef69e6f86ef4c8ea13bc1b04e6ce1865
Blueonics <lannan.jiang@gmail.com>
2025-10-24 12:14:39 +0000

    8bit alu

---
+++ flow/designs/sky130hd/8bit_alu/config.mk
@@ -0,0 +1,19 @@
+export DESIGN_NICKNAME = 8bit_alu
+export DESIGN_NAME = 8bit_alu
+export PLATFORM    = sky130hd
+
+export VERILOG_FILES = $(sort $(wildcard $(DESIGN_HOME)/src/$(DESIGN_NICKNAME)/*.v))
+export VERILOG_INCLUDE_DIRS = $(DESIGN_HOME)/src/$(DESIGN_NICKNAME)/include
+export SDC_FILE      = $(DESIGN_HOME)/$(PLATFORM)/$(DESIGN_NICKNAME)/constraint.sdc
+
+export CORE_UTILIZATION = 50
+export PLACE_DENSITY_LB_ADDON = 0.15
+export TNS_END_PERCENT = 100
+
+export FASTROUTE_TCL = $(DESIGN_HOME)/$(PLATFORM)/$(DESIGN_NICKNAME)/fastroute.tcl
+
+export REMOVE_ABC_BUFFERS = 1
+
+# workaround for density growing to 0.91 from adjustments on TD/RD iterations
+# export GPL_ROUTABILITY_DRIVEN = 0
+
